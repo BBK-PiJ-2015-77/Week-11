29826c7552a02e525480cbd70538edd43fb0ff87
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-01-09 15:46:56 +0000

    Added some more methods

--- GenStackElement.java
+++ GenStackElement.java
@@ -2,6 +2,7 @@ public class GenStackElement<T extends Number> {
 
 	private T value;
 	private GenStackElement nextElement;
+	private int index = 0;
 	
 	public GenStackElement (T value) {
 		this.value = value;
@@ -14,6 +15,32 @@ public class GenStackElement<T extends Number> {
 	
 	public GenStackElement getNext() {
 		return nextElement;
+	}
+	
+	public void push(GenStackElement newElement) {
+		if (this.nextElement == null) {
+			this.nextElement = newElement;
+			index++;
+		} else {
+			this.nextElement.push(newElement);
+		}
+	}
+	
+	public void pop() {
+		if (this.nextElement.nextElement == null) {
+			this.nextElement = null;
+			index--;
+		} else {
+			this.nextElement.pop();
+		}
+	}
+	
+	public boolean isEmpty() {
+		if (index == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
